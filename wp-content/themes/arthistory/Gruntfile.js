88905c24ac4c11f15d8f90a2ae5f9a5e1285bde3
mstumpf818 <mstumpf@ellefsontech.com>
2016-05-09 21:07:59 -0400

    core - customized theme; added grunt workflow

---
+++ wp-content/themes/arthistory/Gruntfile.js
@@ -0,0 +1,162 @@
+/*jslint node: true */
+'use strict';
+
+module.exports = function(grunt){
+    // load all grunt tasks matching the ['grunt-*', '@*/grunt-*'] patterns
+    require('load-grunt-tasks')(grunt);
+    var watchFiles = {
+        mainJs: [
+            'js/**/*.js'
+        ],
+        mainSass: [
+            'scss/**/*.scss'
+        ],
+        handlebars: [
+            'handlebars/**/*.hbs'
+        ]
+    };
+    grunt.initConfig({
+        handlebars: {
+            compile: {
+                src: watchFiles.handlebars,
+                dest: 'build/baobao.handlebars.min.js'
+            },
+            options: {
+                namespace: 'Handlebars.templates',
+                processName: function(filePath) {
+                    var pathPieces = filePath.split('/'),//get filename from path
+                        filePieces = pathPieces[pathPieces.length-1].split('.');//return name of file without extension
+                    return filePieces[0];
+                }
+            }
+        },
+        jshint: {
+            main: {
+                src: [
+                    watchFiles.mainJs
+                ],
+                options: {
+                    jshintrc: true
+                }
+            }
+        },
+        postcss: {
+            base: {
+                options: {
+                    map: true, // inline sourcemaps,
+                    processors: [
+                        require('autoprefixer')({browsers: 'last 2 versions'}) // add vendor prefixes
+                    ]
+                },
+                dist: {
+                    src: 'build/app.base.min.css'
+                }
+            },
+            main: {
+                options: {
+                    map: true, // inline sourcemaps,
+                    processors: [
+                        require('autoprefixer')({browsers: 'last 2 versions'}) // add vendor prefixes
+                    ]
+                },
+                dist: {
+                    src: 'build/app.main.min.css'
+                }
+            }
+        },
+        sass: {
+            base: {
+                files: {
+                    'build/app.base.min.css': 'scss/app_base.scss'
+                },
+                options: {
+                    style: 'compressed',
+                    trace: true
+                }
+            },
+            main: {
+                files: {
+                    'build/app.main.min.css': 'scss/app_main.scss'
+                },
+                options: {
+                    style: 'compressed',
+                    trace: true
+                }
+            }
+        },
+        uglify: {
+            base: {
+                files: {
+                    'build/app.base.min.js': [
+                        'bower_components/bluebird/js/browser/bluebird.min.js',
+                        'bower_components/velocity/velocity.ui.min.js'
+                    ]
+                },
+                options: {
+                    banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    sourceMap: true,
+                    preserveComments: false,
+                    compress: true,
+                    mangle: false
+                }
+            },
+            main: {
+                files: {
+                    'build/app.main.min.js': [
+                        'js/app.js',
+                        'js/lib/*.js',
+                        'js/modules/*.js',
+                        'js/templates/theme/shared.js',
+                        'js/**/*.js'
+                    ]
+                },
+                options: {
+                    banner: '/*! <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                    sourceMap: true,
+                    preserveComments: 'some',
+                    mangle: false
+                }
+            }
+        },
+        watch: {
+            mainJs: {
+                files: watchFiles.mainJs,
+                tasks: ['jshint:main','uglify:main'],
+                options: {
+                    livereload: true
+                }
+            },
+            mainSass: {
+                files: watchFiles.mainSass,
+                tasks: ['sass:main','postcss:main'],
+                options: {
+                    livereload: true
+                }
+            },
+            handlebars: {
+                files: watchFiles.handlebars,
+                tasks: ['handlebars:compile','uglify:main'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+    });
+
+    // Dev task, run jshint, copy custom client side js scripts, then start server and watch
+    grunt.registerTask('dev', [
+        'newer:handlebars:compile',
+        'newer:uglify',
+        'newer:sass',
+        'watch'
+    ]);
+
+    // build task, for initializing environment after clone or UI dependencies update
+    grunt.registerTask('build', [
+        'handlebars:compile',
+        'uglify',
+        'sass',
+        'postcss'
+    ]);
+
+};
--- wp-content/themes/arthistory/template-parts/content.php
+++ wp-content/themes/arthistory/template-parts/content.php
@@ -16,14 +16,7 @@
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php arthistory_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
+			} ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
@@ -40,8 +33,4 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php arthistory_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
