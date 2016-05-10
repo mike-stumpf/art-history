13ec6fcc74012899730aa8ce714d6c82c60ee75a
mstumpf818 <mstumpf@ellefsontech.com>
2016-05-09 22:19:38 -0400

    admin - added custom css to clean up admin panel

--- wp-content/themes/arthistory/functions.php
+++ wp-content/themes/arthistory/functions.php
@@ -4,10 +4,26 @@
  * Enqueue scripts and styles.
  */
 function arthistory_scripts() {
-	wp_enqueue_style( 'arthistory-style-base', get_template_directory_uri().'/build/app.base.min.css' );
+    wp_enqueue_style( 'arthistory-style-base', get_template_directory_uri().'/build/app.base.min.css' );
     wp_enqueue_style( 'arthistory-style-main', get_template_directory_uri().'/build/app.main.min.css' );
     wp_enqueue_script( 'arthistory-logic-base', get_template_directory_uri().'/build/app.base.min.js');
-	wp_enqueue_script( 'arthistory-logic-main', get_template_directory_uri().'/build/app.main.min.js', array(), '20160509', true );
+    wp_enqueue_script( 'arthistory-logic-main', get_template_directory_uri().'/build/app.main.min.js', array(), '20160509', true );
 
 }
 add_action( 'wp_enqueue_scripts', 'arthistory_scripts' );
+
+/*
+ * custom admin css
+ */
+function arthistory_admin_css() { ?>
+    <style>
+        #menu-comments, #menu-tools, /* sidebar */
+        #wp-admin-bar-wp-logo, #wp-admin-bar-comments, /*top bar*/
+        #contextual-help-link-wrap    /* misc */
+        {
+            display: none!important;
+        }
+    </style>
+<?php }
+
+add_action('admin_head', 'arthistory_admin_css');
