ebd12e170b8e7a0eee1c4eb2d88f628c80c1ab3f
mstumpf818 <mstumpf@ellefsontech.com>
2016-05-17 22:27:57 -0400

    core - removed unnecessary template files

--- wp-content/themes/arthistory/header.php
+++ wp-content/themes/arthistory/header.php
@@ -15,6 +15,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
+    <!--    todo, favicon-->
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
