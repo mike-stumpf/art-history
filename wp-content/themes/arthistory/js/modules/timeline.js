242281486e6d62b815f886cdfa32a1c2d19dd297
mike-stumpf <mike.a.stumpf@gmail.com>
2016-11-18 09:30:12 +0000

    timeline - bug fix for non-relative paths for modals

--- wp-content/themes/arthistory/js/modules/timeline.js
+++ wp-content/themes/arthistory/js/modules/timeline.js
@@ -58,7 +58,7 @@
     }
     
     function openModal(){
-        window.location.href = window.location.href+'/#timeline-mobile-modal';
+        window.location.href = '#timeline-mobile-modal';
     }
 
 
